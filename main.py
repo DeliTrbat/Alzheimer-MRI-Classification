5f051aad635b8166994fb68bcb3b3ed5997e0df9
TudFilip <filiptudormihail@yahoo.com>
2023-01-26 02:26:31 +0200

    ultimul commit pana sa rulez toate modelele

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import numpy as np
-import os
-import PIL
-import PIL.Image
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import pathlib
 from functools import partial
 
 # uncomment this if you want to show debug information
@@ -15,6 +10,7 @@ DATASET_FROM_GEORGE = "../AugmentedDataset"
 DEFAULT_DATASET_PATH = DATASET_FROM_GEORGE
 
 EPOCHS = 10
+VGG16_INCEPTION_V3_RESNET50_EPOCHS = 5
 
 BATCH_SIZE = 64
 VGG16_MODEL_BATCH_SIZE = 8
@@ -31,6 +27,12 @@ RGB_DEPTH = 3
 def change_batch_size(new_batch_size):
     global BATCH_SIZE
     BATCH_SIZE = new_batch_size
+
+
+def change_epoch(new_epoch):
+    global EPOCHS
+    EPOCHS = new_epoch
+
 
 def init_train_dataset():
     return tf.keras.utils.image_dataset_from_directory(
@@ -59,12 +61,11 @@ def init_test_dataset():
     )
 
 
-TRAIN_DATASET = init_train_dataset()
-VALIDATION_DATASET = init_validation_dataset()
-TEST_DATASET = init_test_dataset()
-
-
 def plot_some_images_from_train_ds():
+    TRAIN_DATASET = init_train_dataset()
+    VALIDATION_DATASET = init_validation_dataset()
+    TEST_DATASET = init_test_dataset()
+
     plt.figure(figsize=(10, 10))
     class_names = TRAIN_DATASET.class_names
     for images, labels in TRAIN_DATASET.take(1):
@@ -419,14 +420,17 @@ def choose_model():
     elif user_choice == 3:
         print("VGG16 model")
         change_batch_size(VGG16_MODEL_BATCH_SIZE)
+        change_epoch(VGG16_INCEPTION_V3_RESNET50_EPOCHS)
         return create_model_vgg16()
     elif user_choice == 4:
         print("Inception V3 model")
         change_batch_size(INCEPTION_V3_MODEL_BATCH_SIZE)
+        change_epoch(VGG16_INCEPTION_V3_RESNET50_EPOCHS)
         return create_model_inception_v3()
     elif user_choice == 5:
         print("ResNet50 model")
         change_batch_size(RESNET50_MODEL_BATCH_SIZE)
+        change_epoch(VGG16_INCEPTION_V3_RESNET50_EPOCHS)
         return create_model_resnet50()
     elif user_choice == 6:
         print("Exit")
@@ -449,10 +453,11 @@ def run_all_models_and_save_results():
         simple_model.save('./saved_models/simple_model_activation_{0}_epochs_{1}.h5'.format(activation_type, EPOCHS))
         with open('./results/simple_model_activation_{0}_epochs_{1}.txt'.format(activation_type, EPOCHS), 'w') as f:
             f.write("Activation type: {0}\n".format(activation_type))
-            f.write("\t-> Accuracy: {0}\n".format(str(simple_model.history.history['accuracy'])))
-            f.write("\t-> Loss: {0}\n".format(str(simple_model.history.history['loss'])))
-            f.write("\t-> Validation accuracy: {0}\n".format(str(simple_model.history.history['val_accuracy'])))
-            f.write("\t-> Validation loss: {0}\n".format(str(simple_model.history.history['val_loss'])))
+            f.write("\t-> Accuracy: {0}\n".format(str(simple_model.history.history['accuracy'][EPOCHS - 1])))
+            f.write("\t-> Loss: {0}\n".format(str(simple_model.history.history['loss'][EPOCHS - 1])))
+            f.write("\t-> Validation accuracy: {0}\n".format(str(simple_model.history.history['val_accuracy'][EPOCHS - 1])))
+            f.write("\t-> Validation loss: {0}\n".format(str(simple_model.history.history['val_loss'][EPOCHS - 1])))
+        del simple_model
 
     print("Custom models...")
     for activation_type in activation_types:
@@ -462,50 +467,57 @@ def run_all_models_and_save_results():
         custom_model.save('./saved_models/custom_model_activation_{0}_epochs_{1}.h5'.format(activation_type, EPOCHS))
         with open('./results/custom_model_activation_{0}_epochs_{1}.txt'.format(activation_type, EPOCHS), 'w') as f:
             f.write("Activation type: {0}\n".format(activation_type))
-            f.write("\t-> Accuracy: {0}\n".format(str(custom_model.history.history['accuracy'])))
-            f.write("\t-> Loss: {0}\n".format(str(custom_model.history.history['loss'])))
-            f.write("\t-> Validation accuracy: {0}\n".format(str(custom_model.history.history['val_accuracy'])))
-            f.write("\t-> Validation loss: {0}\n".format(str(custom_model.history.history['val_loss'])))
+            f.write("\t-> Accuracy: {0}\n".format(str(custom_model.history.history['accuracy'][EPOCHS - 1])))
+            f.write("\t-> Loss: {0}\n".format(str(custom_model.history.history['loss'][EPOCHS - 1])))
+            f.write("\t-> Validation accuracy: {0}\n".format(str(custom_model.history.history['val_accuracy'][EPOCHS - 1])))
+            f.write("\t-> Validation loss: {0}\n".format(str(custom_model.history.history['val_loss'][EPOCHS - 1])))
+        del custom_model
 
     try:
         print("VGG16 model")
         change_batch_size(VGG16_MODEL_BATCH_SIZE)
+        change_epoch(VGG16_INCEPTION_V3_RESNET50_EPOCHS)
         vgg16 = create_model_vgg16()
         train_model(vgg16)
         vgg16.save('./saved_models/vgg16_model_epochs_{0}.h5'.format(EPOCHS))
         with open('./results/vgg16.txt', 'w') as f:
-            f.write("\t-> Accuracy: {0}\n".format(str(vgg16.history.history['accuracy'])))
-            f.write("\t-> Loss: {0}\n".format(str(vgg16.history.history['loss'])))
-            f.write("\t-> Validation accuracy: {0}\n".format(str(vgg16.history.history['val_accuracy'])))
-            f.write("\t-> Validation loss: {0}\n".format(str(vgg16.history.history['val_loss'])))
+            f.write("\t-> Accuracy: {0}\n".format(str(vgg16.history.history['accuracy'][EPOCHS - 1])))
+            f.write("\t-> Loss: {0}\n".format(str(vgg16.history.history['loss'][EPOCHS - 1])))
+            f.write("\t-> Validation accuracy: {0}\n".format(str(vgg16.history.history['val_accuracy'][EPOCHS - 1])))
+            f.write("\t-> Validation loss: {0}\n".format(str(vgg16.history.history['val_loss'][EPOCHS - 1])))
+        del vgg16
     except Exception:
         print("VGG16 model failed!")
 
     try:
         print("Inception V3 model")
         change_batch_size(INCEPTION_V3_MODEL_BATCH_SIZE)
+        change_epoch(VGG16_INCEPTION_V3_RESNET50_EPOCHS)
         inception_v3 = create_model_inception_v3()
         train_model(inception_v3)
         inception_v3.save('./saved_models/inception_v3_model_epochs_{0}.h5'.format(EPOCHS))
         with open('./results/inception_v3.txt', 'w') as f:
-            f.write("\t-> Accuracy: {0}\n".format(str(inception_v3.history.history['accuracy'])))
-            f.write("\t-> Loss: {0}\n".format(str(inception_v3.history.history['loss'])))
-            f.write("\t-> Validation accuracy: {0}\n".format(str(inception_v3.history.history['val_accuracy'])))
-            f.write("\t-> Validation loss: {0}\n".format(str(inception_v3.history.history['val_loss'])))
+            f.write("\t-> Accuracy: {0}\n".format(str(inception_v3.history.history['accuracy'][EPOCHS - 1])))
+            f.write("\t-> Loss: {0}\n".format(str(inception_v3.history.history['loss'][EPOCHS - 1])))
+            f.write("\t-> Validation accuracy: {0}\n".format(str(inception_v3.history.history['val_accuracy'][EPOCHS - 1])))
+            f.write("\t-> Validation loss: {0}\n".format(str(inception_v3.history.history['val_loss'][EPOCHS - 1])))
+        del inception_v3
     except Exception:
         print("Inception V3 model failed!")
 
     try:
         print("ResNet50 model")
         change_batch_size(RESNET50_MODEL_BATCH_SIZE)
+        change_epoch(VGG16_INCEPTION_V3_RESNET50_EPOCHS)
         resnet50 = create_model_resnet50()
         train_model(resnet50)
         resnet50.save('./saved_models/resnet50_model_epochs_{0}.h5'.format(EPOCHS))
         with open('./results/resnet50.txt', 'w') as f:
-            f.write("\t-> Accuracy: {0}\n".format(str(resnet50.history.history['accuracy'])))
-            f.write("\t-> Loss: {0}\n".format(str(resnet50.history.history['loss'])))
-            f.write("\t-> Validation accuracy: {0}\n".format(str(resnet50.history.history['val_accuracy'])))
-            f.write("\t-> Validation loss: {0}\n".format(str(resnet50.history.history['val_loss'])))
+            f.write("\t-> Accuracy: {0}\n".format(str(resnet50.history.history['accuracy'][EPOCHS - 1])))
+            f.write("\t-> Loss: {0}\n".format(str(resnet50.history.history['loss'][EPOCHS - 1])))
+            f.write("\t-> Validation accuracy: {0}\n".format(str(resnet50.history.history['val_accuracy'][EPOCHS - 1])))
+            f.write("\t-> Validation loss: {0}\n".format(str(resnet50.history.history['val_loss'][EPOCHS - 1])))
+        del resnet50
     except Exception:
         print("ResNet50 model failed!")
 
@@ -515,6 +527,10 @@ def run_all_models_and_save_results():
 
 
 def train_model(my_model):
+    TRAIN_DATASET = init_train_dataset()
+    VALIDATION_DATASET = init_validation_dataset()
+    TEST_DATASET = init_test_dataset()
+
     my_model.fit(
         TRAIN_DATASET,
         validation_data=VALIDATION_DATASET,
